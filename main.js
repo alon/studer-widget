3a768ce299db514f6ef7977c504140f8ca257b3c
Alon Levy <alon@pobox.com>
2017-11-14 10:17:25 +0200

    initial working chart

--- main.js
+++ main.js
@@ -17,53 +17,110 @@ Vue.component("line-chart", {
     }
 });
 
-window.App = new Vue({
-  el: '#app',
-  data: {
-    chartData: [
-    {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
+function parse_csv(data)
+{
+    var lines = data.split('\n');
+    var header = lines[0].split(',');
+    // lines 1 and 2 are not important
+    var extra_header = lines.slice(1, 3);
+    var cells = lines.slice(3).map(function (line) { return line.split(','); });
+    var n = cells[0].length;
+    var last = 0;
+    // remove trailing lines - they contain different information
+    for (var i = cells.length - 1; i > 0; --i) {
+        var cur = cells[i].length;
+        if (cur === n)
         {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: [40, 39, 10, 40, 39, 80, 40]
+            last = i + 1;
+            break;
+        }
+    }
+    var trailer = cells.slice(last);
+    cells = cells.slice(0, last);
+    // must be a oneliner somewhere to do this
+    var ret = {};
+    // skip first cell - it is just the version
+    var titles = ['time'].concat(header.slice(1));
+    for (var i in titles) {
+        var title = titles[i];
+        var col = cells.map(function (row) { return row[i]; });
+        ret[title] = col;
+    }
+    ret.csv_version = header[0];
+    ret.titles = titles;
+    ret.extra_header = extra_header;
+    ret.trailer = trailer;
+    return ret;
+}
+
+
+function create_graph(response)
+{
+    window.Response = response; // for debugging
+    var data = parse_csv(response.data);
+    window.Data = data;
+    var label_uin = 'XT-Uin [Vac]';
+    var label_iin = 'XT-Iin [Aac]';
+    var time = data.time;
+    var colors = ['#f87979', '#88f939'];
+    var datasets = [];
+    var interesting_indices = [1, 4, 5];
+    for (var i = 0 ; i < interesting_indices.length ; ++i) {
+        var title = data.titles[interesting_indices[i]];
+        datasets.push({
+            label: title,
+            color: colors[i % colors.length],
+            data: data[title]
+        });
+    }
+    window.Datasets = datasets;
+    window.App = new Vue({
+      el: '#app',
+      template: '<div>\n' +
+                '  <p id="summary">{{ chart_summary }}</p>\n' +
+                '    <line-chart\n' +
+                '      :chart-data="chartData"\n' +
+                '      :chart-options="chartOptions"/>\n' +
+                '</div>'
+      ,
+      data: {
+        time: time,
+        csv_version: data.csv_version,
+        chartData:
+        {
+          labels: time,
+          datasets: datasets,
         },
-        {
-          label: 'Data Two',
-          backgroundColor: '#88f939',
-          data: [20, 29, 40, 50, 29, 50, 30]
+        chartOptions: {
+            responsive: true, maintainAspectRatio: false
+        },
+      },
+      computed: {
+        chart_summary: function() {
+            return "XT Log: " + this.time[0] + ".." + this.time[this.time.length - 1]
+                + " (" + this.csv_version + ")";
+        },
+        second: function() {
+            console.log("second called");
+            return !this.first;
         }
-      ]
-    },
-    {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'Data Two',
-          backgroundColor: '#88f939',
-          data: [20, 29, 40, 50, 29, 50, 30]
-        }
-      ]
-    }
-    ],
-    chartOptions: {
-        responsive: true, maintainAspectRatio: false
-    },
-    first: true
-  },
-  computed: {
-    chart_summary: function() {
-        return "not done yet";
-    },
-    second: function() {
-        console.log("second called");
-        return !this.first;
-    }
-  },
-  methods: {
-      checked: function() {
-          this.first = !this.first;
-      }
-  },
-});
+      },
+      methods: {
+          checked: function() {
+              this.first = !this.first;
+          }
+      },
+    });
+}
+
+// Not sure: show a "waiting" thing first?
+// Right now:
+//  start XHR for CSV
+//   - meanwhile showing nothing
+//  when it arrives, create chart
+//   - which will contain all charts
+
+axios.get('LG160704.CSV')
+    .then(create_graph);
+
+
