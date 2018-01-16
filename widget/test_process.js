0a9cdb263442d984d2a0a173ec08ac4633786efb
Alon Levy <alon@pobox.com>
2018-01-16 09:56:20 +0200

    test_process: test multiple file extraction

--- widget/test_process.js
+++ widget/test_process.js
@@ -1,4 +1,7 @@
-let readFileSync = require('fs').readFileSync;
-let process = require('./process.js').default;
-let data = readFileSync('LG171218.CSV', 'utf8');
-process([{data: data}], 1);
+let fs = require('fs');
+let readFileSync = fs.readFileSync;
+let filenames = fs.readdirSync('.').filter(x => x.substr(x.length - 3, x.length) == 'CSV' && x.substr(0, 4) != 'LG16');
+console.log(filenames);
+let extract_datasets = require('./process.js').default;
+let data = filenames.map(fn => { return {data: readFileSync(fn, 'utf8')}; });
+extract_datasets(data, 10);
