be96b9e8bb3210a39f40a585f5bd09b2209f753c
Alon Levy <alon@trellis.ag>
2019-02-04 19:55:47 +0200

    empty subscription (Browser.element program)

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,21 @@
 module Main exposing (main)
 import Browser
 import Html exposing (..)
+import Http
 
 -- My Main
 
 type alias Model = String
 
 
-init : Model
-init = "hello"
+init : () -> (Model, Cmd Msg)
+init _ =
+  ("hello", Cmd.none)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  model
+  (model, Cmd.none)
 
 
 type Msg =
@@ -25,6 +27,11 @@ view model =
   div [] [ text model ]
 
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
