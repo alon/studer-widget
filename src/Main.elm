c294576115362f8460a358dd3043051981745a9f
Alon Levy <alon@trellis.ag>
2019-02-06 22:47:15 +0200

    download everything

--- src/Main.elm
+++ src/Main.elm
@@ -7,15 +7,23 @@ import Regex exposing (..)
 -- My Main
 
 
+type alias AFile = { filename : String, content : String }
+
+
 type Model =
   Init
-  | Downloading (List String) (List String)
+  | Downloading {
+      next : (List String),
+      current : Maybe String,
+      done : (List AFile)
+    }
   | Error String
+
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  (Init, Http.get { url = "/", expect = Http.expectString GotText })
+  (Init, Http.get { url = "/", expect = Http.expectString GotRoot })
 
 
 parse_hrefs : String -> List String
@@ -38,21 +46,58 @@ flattenList l =
     [] :: y -> flattenList y
 
 
+download : Model -> (Model, Cmd Msg)
+download model =
+  case model of
+    Downloading data ->
+      case data.next of
+        first :: rest ->
+          (Downloading { next = rest, current = Just first, done = data.done },
+           Http.get { url = first, expect = Http.expectString GotFile }
+          )
+        [] ->
+          (model, Cmd.none)
+    _ -> (model, Cmd.none)
+
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Nothing -> (model, Cmd.none)
-    GotText result ->
+    DoNothing -> (model, Cmd.none)
+    GotFile result ->
+      case result of
+        Ok content ->
+          case model of
+            Downloading data ->
+              case data.current of
+                Just current ->
+                  let
+                    newmodel = Downloading { data | done = ({ filename = current, content = content } :: data.done), current = Nothing }
+                  in
+                    download newmodel
+                _ -> (Error "another unexpected case", Cmd.none)
+            _ -> (Error "unexpected endcase, how do I prevent this at compile time?", Cmd.none)
+        Err _ ->
+          case model of
+            Downloading data ->
+              (Error ("error downloading " ++ (Debug.toString data.current)), Cmd.none)
+            _ ->
+              (Error "error downloading and not in Downloading, elm-help!", Cmd.none)
+    GotRoot result ->
       case result of
         Ok something ->
-          (Downloading (parse_hrefs something) [], Cmd.none)
+          let
+              parsed = parse_hrefs something
+          in
+              download (Downloading { done = [], next = parsed, current = Nothing })
         Err _ ->
           (Error "Get error", Cmd.none)
 
 
 type Msg =
-  Nothing
-  | GotText (Result Http.Error String)
+  DoNothing
+  | GotRoot (Result Http.Error String)
+  | GotFile (Result Http.Error String)
 
 
 view : Model -> Html Msg
@@ -60,12 +105,13 @@ view model =
   case model of
     Init -> div [] [text "init"]
     Error s -> div [] [text s]
-    Downloading missing downloaded ->
+    Downloading data ->
       div []
         ([div [] [text "missing"]] ++
-        List.map (\x -> div [] [text x]) missing ++
+        List.map (\x -> div [] [text x]) data.next ++
         [div [] [text "downloaded"]] ++
-        List.map (\x -> div [] [text x]) downloaded
+        List.map (\x -> div [] [text ("downloaded " ++ (x.filename))]) data.done ++
+        [div [] [text (Debug.toString data.current)]]
         )
 
 
