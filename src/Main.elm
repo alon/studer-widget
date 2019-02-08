de12e634f89fe5a81482aaf1725a9736b4bb9949
Alon Levy <alon@trellis.ag>
2019-02-08 22:30:23 +0200

    first working (badly) version with two sub models/msg/view/update

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (main)
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (..)
 import Http
 import Regex exposing (..)
 import File.Download as Download
@@ -14,16 +15,25 @@ import Bytes exposing (Bytes)
 type alias AFile = { filename : String, content : String }
 
 
+type alias DateControlModel =
+  -- todo: Maybe'fy
+  { day: String, month: String, year: String }
+
+
+defaultDateControlModel =
+  { day = "1", month = "1", year = "2019" }
+
+
 type Model =
   Init
   | GettingServerFile {
       next : (List String),
       current : Maybe String,
-      done : (List AFile)
+      done : (List AFile),
+      first : DateControlModel,
+      last : DateControlModel
     }
-  -- TODO: have a state for DoneDownloading, which enables the download button
   | Error String
-
 
 
 init : () -> (Model, Cmd Msg)
@@ -57,7 +67,7 @@ download model =
     GettingServerFile data ->
       case data.next of
         first :: rest ->
-          (GettingServerFile { next = rest, current = Just first, done = data.done },
+          (GettingServerFile { data | next = rest, current = Just first, done = data.done },
            Http.get {
                url = (Debug.log "get url" (first ++ "#bla")),
                expect = Http.expectString GotFile
@@ -77,10 +87,30 @@ tar files =
     createArchive data
 
 
+updateDateControl : DateControlMsg -> DateControlModel -> DateControlModel
+updateDateControl msg model =
+  case msg of
+    UpdateDay day -> { model | day = day }
+    UpdateMonth month -> { model | month = month }
+    UpdateYear year -> { model | year = year }
+
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     DoNothing -> (model, Cmd.none)
+    UpdateFirst submsg ->
+      case model of
+        GettingServerFile data ->
+          (GettingServerFile { data | first = (updateDateControl submsg data.first ) }, Cmd.none)
+        _ ->
+          (model, Cmd.none) -- and error??
+    UpdateLast submsg ->
+      case model of
+        GettingServerFile data ->
+          (GettingServerFile { data | last = (updateDateControl submsg data.last ) }, Cmd.none)
+        _ ->
+          (model, Cmd.none) -- and error??
     DownloadToUser ->
       case model of
         GettingServerFile data ->
@@ -113,9 +143,15 @@ update msg model =
               parsed = parse_hrefs something
               csvs = List.filter (\s -> String.endsWith ".csv" (String.toLower s)) parsed
           in
-              download (GettingServerFile { done = [], next = csvs, current = Nothing })
+              download (GettingServerFile { done = [], next = csvs, current = Nothing, first = defaultDateControlModel, last = defaultDateControlModel })
         Err _ ->
           (Error "Get error", Cmd.none)
+
+
+type DateControlMsg =
+  UpdateDay String
+  | UpdateMonth String
+  | UpdateYear String
 
 
 type Msg =
@@ -123,6 +159,8 @@ type Msg =
   | GotRoot (Result Http.Error String)
   | GotFile (Result Http.Error String)
   | DownloadToUser
+  | UpdateFirst DateControlMsg
+  | UpdateLast DateControlMsg
 
 
 view : Model -> Html Msg
@@ -139,8 +177,42 @@ view model =
         [div [] [text (Debug.toString data.current)]] ++
         case List.length data.done of
           0 -> []
-          _ -> [button [onClick DownloadToUser] [(text "download")]]
+          _ -> downloadDialog model
         )
+
+
+to_csv_name d m y =
+  "bla_" ++ y ++ m ++ d ++ ".csv"
+
+
+in_range d1 m1 y1 d2 m2 y2 name =
+  let
+    min_name = to_csv_name d1 m1 y1
+    max_name = to_csv_name d2 m2 y2
+  in
+    name >= min_name && name <= max_name
+
+
+viewDateControl : DateControlModel -> List (Html DateControlMsg)
+viewDateControl model =
+  [ input [ placeholder "day", value model.day, onInput UpdateDay ] [] ] ++
+  [ input [ placeholder "month", value model.month, onInput UpdateMonth ] [] ] ++
+  [ input [ placeholder "year", value model.year, onInput UpdateYear ] [] ]
+
+
+downloadDialog model =
+  case model of
+    GettingServerFile data ->
+      [ button [onClick DownloadToUser] [(text "download")] ] ++
+      List.map (Html.map UpdateFirst) (viewDateControl data.first) ++
+      List.map (Html.map UpdateLast) (viewDateControl data.last) ++
+        let
+          first = data.first
+          last = data.last
+          in_range_h = in_range first.day first.month first.year last.day last.month last.year
+        in
+          List.map text (List.filter in_range_h (List.map .filename data.done))
+    _ -> []
 
 
 subscriptions : Model -> Sub Msg
