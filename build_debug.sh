95b461f4838b8707db5a6cb49d9c7c24d39a31e7
Alon Levy <alon@trellis.ag>
2019-02-11 10:07:51 +0200

    build_debug.sh: fix for development env with elm in path

--- build_debug.sh
+++ build_debug.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 SRC_DIR=$(dirname ${BASH_SOURCE[0]})
-if [ ! -e $SRC_DIR/node_modules/.bin/elm ]; then
-    echo missing elm in $SRC_DIR
-    exit -1
+if [ ! -e $(which elm) ] ; then
+  if [ ! -e $SRC_DIR/node_modules/.bin/elm ]; then
+      echo missing elm in $SRC_DIR
+      exit -1
+  fi
+  export PATH=$SRC_DIR/node_modules/.bin:$PATH
 fi
-export PATH=$SRC_DIR/node_modules/.bin:$PATH
 elm make src/Main.elm --output elm.js
